e8e77afde382d5dfd7e61a7ac273431f54459c45
Yaroslav Myrka <xmyrka@stuba.sk>
2025-11-08 17:49:42 +0100

    initialize spring boot app

---
+++ csp/src/test/java/com/vitalioleksenko/csp/CspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitalioleksenko.csp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
